eab056dbd32dc53f66168361b0a13bdf8fbf2ef4
Sagar Sudhakar <sagar.sudhakar@artisanpartners.com>
2022-06-01 23:11:46 -0500

    Initial commit

---
+++ src/main/java/com/sagar/App.java
@@ -0,0 +1,13 @@
+package com.sagar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
